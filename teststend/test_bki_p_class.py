54aac1122712194ff9116422bf665b080173c436
irik_safin <osiris365@gmail.com>
2022-02-28 01:34:30 +0500

    функции разбиты на более мелкие функции, глобальные переменные переведены в локальные, добавленны аннотации типов, == переделан в is, изменен цвет всплывающего окна при неисправности, добавлено исключение при отсутствии связи с контроллером

--- teststend/test_bki_p_class.py
+++ teststend/test_bki_p_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Алгоритм проверки
 Тип блока	Производитель
@@ -11,6 +12,7 @@
 
 from sys import exit
 from time import sleep
+
 from gen_func_utils import *
 from my_msgbox import *
 from gen_mb_client import *
@@ -18,64 +20,79 @@ from gen_mysql_connect import *
 
 __all__ = ["TestBKIP"]
 
-reset = ResetRelay()
-resist = Resistor()
-ctrl_kl = CtrlKL()
-read_mb = ReadMB()
-mysql_conn = MySQLConnect()
-fault = Bug(None)
-
 
 class TestBKIP(object):
+
+    __resist = Resistor()
+    __ctrl_kl = CtrlKL()
+    __read_mb = ReadMB()
+    __mysql_conn = MySQLConnect()
+    __fault = Bug(None)
+
     def __init__(self):
         pass
     
-    def st_test_bki_p(self):
-        # reset.reset_all()
-        in_0, in_1 = self.__inputs_a()
-        if in_0 == True and in_1 == False: 
+    def st_test_1_bki_p(self):
+        """
+        Тест 1. Проверка исходного состояния блока
+        """
+        in_a0, in_a1 = self.__inputs_a()
+        if in_a0 is True and in_a1 is False:
             pass
-        elif in_0 == False or in_1 == True:
-            mysql_conn.mysql_ins_result("неисправен", "1")
-            mysql_conn.mysql_error(30)
+        elif in_a0 is False or in_a1 is True:
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
+            self.__mysql_conn.mysql_error(30)
             return False
-        mysql_conn.mysql_ins_result("исправен", "1")
-        # Тест 2. Проверка работы блока при нормальном сопротивлении изоляции
-        # Сообщение	Переведите тумблер на блоке в режим «Предупредительный»
+        self.__mysql_conn.mysql_ins_result("исправен", "1")
+        return True
+
+    def st_test_20_bki_p(self) -> bool:
+        """
+        Тест 2. Проверка работы блока при нормальном сопротивлении изоляции
+        """
         msg_1 = 'Переведите тумблер на блоке в режим «Предупредительный»'
         if my_msg(msg_1):
             pass
         else:
             return False
-        ctrl_kl.ctrl_relay('KL21', True)
+        self.__ctrl_kl.ctrl_relay('KL21', True)
         sleep(2)
-        resist.resist_kohm(220)
+        self.__resist.resist_kohm(220)
         sleep(2)
-        in_0, in_1 = self.__inputs_a()
-        if in_0 == True and in_1 == False:
+        in_a0, in_a1 = self.__inputs_a()
+        if in_a0 is True and in_a1 is False:
             pass
-        elif in_0 == False or in_1 == True:
-            mysql_conn.mysql_ins_result("неисправен", "2")
-            mysql_conn.mysql_error(31)
+        elif in_a0 is False or in_a1 is True:
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__mysql_conn.mysql_error(31)
             return False
-        mysql_conn.mysql_ins_result("исправен", "2")
-        # Тест 3. Проверка работы блока в режиме «Предупредительный» при снижении
-        # уровня сопротивлении изоляции до 100 кОм
-        resist.resist_220_to_100_kohm()
-        b = ctrl_kl.ctrl_ai_code_100()
+        self.__mysql_conn.mysql_ins_result("исправен", "2")
+        return True
+
+    def st_test_30_bki_p(self) -> bool:
+        """
+        Тест 3. Проверка работы блока в режиме «Предупредительный» при снижении
+        уровня сопротивлении изоляции до 100 кОм
+        """
+        self.__resist.resist_220_to_100_kohm()
+        b = self.__ctrl_kl.ctrl_ai_code_100()
         i = 0
         while b == 2 or i <= 10:
             sleep(0.2)
             i += 1
-            b = ctrl_kl.ctrl_ai_code_100()
-            if b == 0:
+            b = self.__ctrl_kl.ctrl_ai_code_100()
+            if b is 0:
                 break
             elif b == 1:
-                mysql_conn.mysql_error(32)
+                self.__mysql_conn.mysql_error(32)
                 return False
-        mysql_conn.mysql_ins_result("исправен", "3")
-        # Тест 4. Проверка работы блока в режиме «Аварийный» при сопротивлении изоляции 100 кОм
-        # Сообщение	Переведите тумблер на блоке в режим «Аварийный»
+        self.__mysql_conn.mysql_ins_result("исправен", "3")
+        return True
+
+    def st_test_40_bki_p(self) -> bool:
+        """
+        Тест 4. Проверка работы блока в режиме «Аварийный» при сопротивлении изоляции 100 кОм
+        """
         msg_2 = 'Переведите тумблер на блоке в режим «Аварийный»'
         if my_msg(msg_2):
             pass
@@ -83,50 +100,70 @@ class TestBKIP(object):
             return False
         sleep(2)
         in_a0, in_a1 = self.__inputs_a()
-        if in_a0 == True and in_a1 == False:
+        if in_a0 is True and in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", "4")
-            mysql_conn.mysql_error(33)
-            ctrl_kl.ctrl_relay('KL21', False)
+            self.__mysql_conn.mysql_ins_result("неисправен", "4")
+            self.__mysql_conn.mysql_error(33)
+            self.__ctrl_kl.ctrl_relay('KL21', False)
             return False
-        mysql_conn.mysql_ins_result("исправен", "4")
-        # Тест 5. Работа блока в режиме «Аварийный» при сопротивлении изоляции
-        # ниже 30 кОм (Подключение на внутреннее сопротивление)
-        # resist.resist_kohm(220)
-        ctrl_kl.ctrl_relay('KL22', True)
+        self.__mysql_conn.mysql_ins_result("исправен", "4")
+        return True
+
+    def st_test_50_bki_p(self) -> bool:
+        """
+        Тест 5. Работа блока в режиме «Аварийный» при сопротивлении изоляции
+        ниже 30 кОм (Подключение на внутреннее сопротивление)
+        """
+        self.__ctrl_kl.ctrl_relay('KL22', True)
         in_a0, in_a1 = self.__inputs_a()
-        if in_a0 == False and in_a1 == True:
+        if in_a0 is False and in_a1 is True:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", "5")
-            mysql_conn.mysql_error(34)
+            self.__mysql_conn.mysql_ins_result("неисправен", "5")
+            self.__mysql_conn.mysql_error(34)
     
             return False
-        ctrl_kl.ctrl_relay('KL21', False)
-        mysql_conn.mysql_ins_result("исправен", "5")
+        self.__ctrl_kl.ctrl_relay('KL21', False)
+        self.__mysql_conn.mysql_ins_result("исправен", "5")
         return True
     
-    @staticmethod
-    def __inputs_a():
-        in_0 = read_mb.read_discrete(0)
-        in_1 = read_mb.read_discrete(1)
-        return in_0, in_1
+    def __inputs_a(self):
+        in_a0 = self.__read_mb.read_discrete(0)
+        in_a1 = self.__read_mb.read_discrete(1)
+        if in_a0 is None or in_a1 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
+        return in_a0, in_a1
+
+    def st_test_bki_p(self) -> bool:
+        if self.st_test_1_bki_p():
+            if self.st_test_20_bki_p():
+                if self.st_test_30_bki_p():
+                    if self.st_test_40_bki_p():
+                        if self.st_test_50_bki_p():
+                            return True
+        return False
 
 
 if __name__ == '__main__':
+    test_bki_p = TestBKIP()
+    reset_test_bki_p = ResetRelay()
+    mysql_conn_bki_p = MySQLConnect()
+    fault = Bug(True)
     try:
-        test_bki_p = TestBKIP()
         if test_bki_p.st_test_bki_p():
-            mysql_conn.mysql_block_good()
+            mysql_conn_bki_p.mysql_block_good()
             my_msg('Блок исправен')
         else:
-            mysql_conn.mysql_block_bad()
-            my_msg('Блок неисправен')
+            mysql_conn_bki_p.mysql_block_bad()
+            my_msg('Блок неисправен', '#A61E1E')
     except OSError:
         my_msg("ошибка системы")
     except SystemError:
         my_msg("внутренняя ошибка")
+    except ModbusConnectException as mce:
+        fault.debug_msg(mce, 1)
+        my_msg(str(mce), '#A61E1E')
     finally:
-        reset.reset_all()
+        reset_test_bki_p.reset_all()
         exit()
