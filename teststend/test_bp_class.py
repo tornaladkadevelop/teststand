29df0991479dde75df674d8b908e06491b10d17c
irik_safin <osiris365@gmail.com>
2022-02-28 13:49:00 +0500

    функции разбиты на более мелкие функции, глобальные переменные переведены в локальные, добавленны аннотации типов, == переделан в is, изменен цвет всплывающего окна при неисправности, добавлено исключение при отсутствии связи с контроллером

--- teststend/test_bp_class.py
+++ teststend/test_bp_class.py
@@ -16,208 +16,232 @@ import math
 
 from sys import exit
 from time import sleep
+
 from my_msgbox import *
-from gen_func_procedure import *
 from gen_func_utils import *
 from gen_mb_client import *
 from gen_mysql_connect import *
 
 __all__ = ["TestBP"]
 
-proc = Procedure()
-reset = ResetRelay()
-resist = Resistor()
-mb_ctrl = CtrlKL()
-read_mb = ReadMB()
-mysql_conn = MySQLConnect()
-fault = Bug(True)
-
 
 class TestBP(object):
+
+    __mb_ctrl = CtrlKL()
+    __read_mb = ReadMB()
+    __mysql_conn = MySQLConnect()
+    __fault = Bug(True)
+
+    emkost_kond: float
+    emkost_kond_d: float
+
     def __init__(self):
         pass
     
-    def st_test_bp(self):
-        # reset.reset_all()
-        # Тест 1. Проверка исходного состояния блока:
-        # Переключение АЦП на AI.1 канал
+    def st_test_10_bp(self) -> bool:
+        """
+        Тест 1. Проверка исходного состояния блока:
+        Переключение АЦП на AI.1 канал
+        """
         msg_1 = "Убедитесь в отсутствии других блоков и вставьте блок БП в соответствующий разъем"
         if my_msg(msg_1):
             pass
         else:
             return False
-        mysql_conn.mysql_ins_result("идёт тест 1", "1")
-        fault.debug_msg("тест 1", 3)
-        mb_ctrl.ctrl_relay('KL78', True)
+        self.__mysql_conn.mysql_ins_result("идёт тест 1", "1")
+        self.__fault.debug_msg("тест 1", 3)
+        self.__mb_ctrl.ctrl_relay('KL78', True)
         in_a1, in_a2, in_a6, in_a7 = self.__inputs_a()
-        fault.debug_msg(f'{in_a1=}\t{in_a2=}\t{in_a6=}\t{in_a7=}', 3)
-        if in_a6 == True and in_a1 == False and in_a7 == True and in_a2 == False:
+        self.__fault.debug_msg(f'{in_a1=}\t{in_a2=}\t{in_a6=}\t{in_a7=}', 3)
+        if in_a6 is True and in_a1 is False and in_a7 is True and in_a2 is False:
             pass
         else:
-            fault.debug_msg("тест 1 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "1")
+            self.__fault.debug_msg("тест 1 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
             return False
-        fault.debug_msg("тест 1 положение выходов соответствует", 4)
-        mysql_conn.mysql_ins_result("исправен", "1")
-        # Тест 2. Определение ёмкости пусковых конденсаторов
-        # 2.1. Заряд конденсаторов
-        mysql_conn.mysql_ins_result("идёт тест 2.1", "2")
-        fault.debug_msg("тест 2", 3)
-        mb_ctrl.ctrl_relay('KL77', True)
+        self.__fault.debug_msg("тест 1 положение выходов соответствует", 4)
+        self.__mysql_conn.mysql_ins_result("исправен", "1")
+        return True
+
+    def st_test_20_bp(self) -> bool:
+        """
+        Тест 2. Определение ёмкости пусковых конденсаторов
+        2.1. Заряд конденсаторов
+        """
+        self.__mysql_conn.mysql_ins_result("идёт тест 2.1", "2")
+        self.__fault.debug_msg("тест 2", 3)
+        self.__mb_ctrl.ctrl_relay('KL77', True)
         sleep(0.3)
-        mb_ctrl.ctrl_relay('KL65', True)
+        self.__mb_ctrl.ctrl_relay('KL65', True)
         sleep(0.3)
-        mb_ctrl.ctrl_relay('KL66', True)
+        self.__mb_ctrl.ctrl_relay('KL66', True)
         sleep(5)
-        mb_ctrl.ctrl_relay('KL76', True)
+        self.__mb_ctrl.ctrl_relay('KL76', True)
         sleep(5)
-        zaryad_1 = read_mb.read_analog_ai2()
-        fault.debug_msg(f'заряд конденсатора по истечении 5с:\t{zaryad_1} В', 2)
+        zaryad_1 = self.__read_mb.read_analog_ai2()
+        self.__fault.debug_msg(f'заряд конденсатора по истечении 5с:\t{zaryad_1} В', 2)
         if zaryad_1 != 999:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", "2")
-            fault.debug_msg("тест 2 не пройден", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__fault.debug_msg("тест 2 не пройден", 1)
             return False
         sleep(15)
-        zaryad_2 = read_mb.read_analog_ai2()
-        fault.debug_msg(f'заряд конденсатора по истечении 15с:\t{zaryad_2} В', 2)
+        zaryad_2 = self.__read_mb.read_analog_ai2()
+        self.__fault.debug_msg(f'заряд конденсатора по истечении 15с:\t{zaryad_2} В', 2)
         if zaryad_2 != 999:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", "2")
-            fault.debug_msg("тест 2 не пройден", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__fault.debug_msg("тест 2 не пройден", 1)
             return False
         delta_zaryad = zaryad_1 - zaryad_2
-        fault.debug_msg(f'дельта заряда конденсатора:\t{delta_zaryad} В', 2)
+        self.__fault.debug_msg(f'дельта заряда конденсатора:\t{delta_zaryad} В', 2)
         if delta_zaryad != 0:
             pass
         else:
-            mb_ctrl.ctrl_relay('KL77', False)
+            self.__mb_ctrl.ctrl_relay('KL77', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL65', False)
+            self.__mb_ctrl.ctrl_relay('KL65', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL76', False)
-            mb_ctrl.ctrl_relay('KL66', False)
-            mb_ctrl.ctrl_relay('KL78', False)
-            mysql_conn.mysql_ins_result("неисправен", "2")
-            fault.debug_msg("тест 2 не пройден", 1)
+            self.__mb_ctrl.ctrl_relay('KL76', False)
+            self.__mb_ctrl.ctrl_relay('KL66', False)
+            self.__mb_ctrl.ctrl_relay('KL78', False)
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__fault.debug_msg("тест 2 не пройден", 1)
             return False
-        # С1=ln(volt1/ volt2)
-        emkost_kond = math.log(zaryad_1/zaryad_2)
-        fault.debug_msg(f'ёмкость:\t{emkost_kond}', 2)
-        # C1 = t2/C1/31300
-        emkost_kond = (15000 / emkost_kond / 31300) * 1000
-        fault.debug_msg(f'ёмкость:\t{emkost_kond}', 2)
-        # C1=C1*1000
-        # Cd = 100-100*(C1/2000)
-        emkost_kond_d = 100 - 100 * (emkost_kond / 2000)
-        fault.debug_msg(f'ёмкость:\t{emkost_kond_d}', 2)
-        # C1 ≥ 0.8*2000
-        if emkost_kond >= 1600:
+        self.emkost_kond = math.log(zaryad_1/zaryad_2)
+        self.__fault.debug_msg(f'ёмкость:\t{self.emkost_kond}', 2)
+        self.emkost_kond = (15000 / self.emkost_kond / 31300) * 1000
+        self.__fault.debug_msg(f'ёмкость:\t{self.emkost_kond}', 2)
+        self.emkost_kond_d = 100 - 100 * (self.emkost_kond / 2000)
+        self.__fault.debug_msg(f'ёмкость:\t{self.emkost_kond_d}', 2)
+        if self.emkost_kond >= 1600:
             pass
         else:
-            mb_ctrl.ctrl_relay('KL77', False)
+            self.__mb_ctrl.ctrl_relay('KL77', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL65', False)
+            self.__mb_ctrl.ctrl_relay('KL65', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL76', False)
-            mb_ctrl.ctrl_relay('KL66', False)
-            mb_ctrl.ctrl_relay('KL78', False)
-            mysql_conn.mysql_ins_result(f'неиспр. емкость снижена на {emkost_kond_d:.1f} %', "2")
-            fault.debug_msg("тест 2 не пройден", 1)
+            self.__mb_ctrl.ctrl_relay('KL76', False)
+            self.__mb_ctrl.ctrl_relay('KL66', False)
+            self.__mb_ctrl.ctrl_relay('KL78', False)
+            self.__mysql_conn.mysql_ins_result(f'неиспр. емкость снижена на {self.emkost_kond_d:.1f} %', "2")
+            self.__fault.debug_msg("тест 2 не пройден", 1)
             return False
         # 2.3. Форсированный разряд
-        mysql_conn.mysql_ins_result("идёт тест 2.3", "2")
-        fault.debug_msg("тест 2.3", 3)
-        mb_ctrl.ctrl_relay('KL79', True)
+        self.__mysql_conn.mysql_ins_result("идёт тест 2.3", "2")
+        self.__fault.debug_msg("тест 2.3", 3)
+        self.__mb_ctrl.ctrl_relay('KL79', True)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL79', False)
+        self.__mb_ctrl.ctrl_relay('KL79', False)
         sleep(0.3)
-        mysql_conn.mysql_ins_result("исправен", "2")
-        mysql_conn.mysql_ins_result(f'{emkost_kond:.1f}', "3")
-        mysql_conn.mysql_ins_result(f'{emkost_kond_d:.1f}', "4")
-        # Тест 3. Проверка работоспособности реле удержания
-        mysql_conn.mysql_ins_result("идёт тест 3", "5")
-        fault.debug_msg("тест 3", 3)
-        mb_ctrl.ctrl_relay('KL75', True)
+        self.__mysql_conn.mysql_ins_result("исправен", "2")
+        self.__mysql_conn.mysql_ins_result(f'{self.emkost_kond:.1f}', "3")
+        self.__mysql_conn.mysql_ins_result(f'{self.emkost_kond_d:.1f}', "4")
+        return True
+
+    def st_test_30_bp(self) -> bool:
+        """
+        Тест 3. Проверка работоспособности реле удержания
+        """
+        self.__mysql_conn.mysql_ins_result("идёт тест 3", "5")
+        self.__fault.debug_msg("тест 3", 3)
+        self.__mb_ctrl.ctrl_relay('KL75', True)
         sleep(0.3)
         in_a1, in_a2, in_a6, in_a7 = self.__inputs_a()
-        fault.debug_msg(f'{in_a1=}\t{in_a2=}\t{in_a6=}\t{in_a7=}', 3)
-        if in_a6 == False and in_a1 == True and in_a7 == False and in_a2 == True:
+        self.__fault.debug_msg(f'{in_a1=}\t{in_a2=}\t{in_a6=}\t{in_a7=}', 3)
+        if in_a6 is False and in_a1 is True and in_a7 is False and in_a2 is True:
             pass
         else:
-            fault.debug_msg("тест 3 положение выходов не соответствует", 1)
-            mb_ctrl.ctrl_relay('KL77', False)
+            self.__fault.debug_msg("тест 3 положение выходов не соответствует", 1)
+            self.__mb_ctrl.ctrl_relay('KL77', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL65', False)
+            self.__mb_ctrl.ctrl_relay('KL65', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL76', False)
-            mb_ctrl.ctrl_relay('KL66', False)
-            mb_ctrl.ctrl_relay('KL78', False)
-            mb_ctrl.ctrl_relay('KL75', False)
-            mysql_conn.mysql_ins_result("неисправен", "5")
-            fault.debug_msg("тест 3 не пройден", 1)
+            self.__mb_ctrl.ctrl_relay('KL76', False)
+            self.__mb_ctrl.ctrl_relay('KL66', False)
+            self.__mb_ctrl.ctrl_relay('KL78', False)
+            self.__mb_ctrl.ctrl_relay('KL75', False)
+            self.__mysql_conn.mysql_ins_result("неисправен", "5")
+            self.__fault.debug_msg("тест 3 не пройден", 1)
             return False
-        fault.debug_msg("тест 3 положение выходов соответствует", 4)
-        mysql_conn.mysql_ins_result("исправен", "5")
-        # Тест 4. Проверка работоспособности реле удержания
-        # Измерение	A0:AI.1
-        # Вычисление	volt3 = (A0:AI.1)*(103/3)
-        # Состояние правильное	volt3  ≥ 6В
-        mysql_conn.mysql_ins_result("идёт тест 4", "6")
-        fault.debug_msg("тест 4", 3)
-        meas_volt = read_mb.read_analog_ai2()
+        self.__fault.debug_msg("тест 3 положение выходов соответствует", 4)
+        self.__mysql_conn.mysql_ins_result("исправен", "5")
+        return True
+
+    def st_test_40_bp(self) -> bool:
+        """
+        Тест 4. Проверка работоспособности реле удержания
+        """
+        self.__mysql_conn.mysql_ins_result("идёт тест 4", "6")
+        self.__fault.debug_msg("тест 4", 3)
+        meas_volt = self.__read_mb.read_analog_ai2()
         calc_volt = meas_volt * (103 / 3)
-        fault.debug_msg(f'вычисленное напряжение, должно быть больше 6\t{calc_volt}', 2)
+        self.__fault.debug_msg(f'вычисленное напряжение, должно быть больше 6\t{calc_volt}', 2)
         if calc_volt >= 6:
             pass
         else:
-            mb_ctrl.ctrl_relay('KL77', False)
+            self.__mb_ctrl.ctrl_relay('KL77', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL65', False)
+            self.__mb_ctrl.ctrl_relay('KL65', False)
             sleep(0.1)
-            mb_ctrl.ctrl_relay('KL75', False)
-            mb_ctrl.ctrl_relay('KL76', False)
-            mb_ctrl.ctrl_relay('KL66', False)
-            mb_ctrl.ctrl_relay('KL78', False)
-            mysql_conn.mysql_ins_result("неисправен", "6")
-            fault.debug_msg("тест 4 не пройден", 1)
+            self.__mb_ctrl.ctrl_relay('KL75', False)
+            self.__mb_ctrl.ctrl_relay('KL76', False)
+            self.__mb_ctrl.ctrl_relay('KL66', False)
+            self.__mb_ctrl.ctrl_relay('KL78', False)
+            self.__mysql_conn.mysql_ins_result("неисправен", "6")
+            self.__fault.debug_msg("тест 4 не пройден", 1)
             return False
-        mb_ctrl.ctrl_relay('KL77', False)
+        self.__mb_ctrl.ctrl_relay('KL77', False)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL65', False)
+        self.__mb_ctrl.ctrl_relay('KL65', False)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL75', False)
-        mb_ctrl.ctrl_relay('KL76', False)
-        mb_ctrl.ctrl_relay('KL66', False)
-        mb_ctrl.ctrl_relay('KL78', False)
-        mysql_conn.mysql_ins_result("исправен", "6")
-        fault.debug_msg("тест 4 пройден", 4)
+        self.__mb_ctrl.ctrl_relay('KL75', False)
+        self.__mb_ctrl.ctrl_relay('KL76', False)
+        self.__mb_ctrl.ctrl_relay('KL66', False)
+        self.__mb_ctrl.ctrl_relay('KL78', False)
+        self.__mysql_conn.mysql_ins_result("исправен", "6")
+        self.__fault.debug_msg("тест 4 пройден", 4)
         return True
     
-    @staticmethod
-    def __inputs_a():
-        in_a1 = read_mb.read_discrete(1)
-        in_a2 = read_mb.read_discrete(2)
-        in_a6 = read_mb.read_discrete(6)
-        in_a7 = read_mb.read_discrete(7)
+    def __inputs_a(self):
+        in_a1 = self.__read_mb.read_discrete(1)
+        in_a2 = self.__read_mb.read_discrete(2)
+        in_a6 = self.__read_mb.read_discrete(6)
+        in_a7 = self.__read_mb.read_discrete(7)
+        if in_a1 is None or in_a2 is None or in_a6 is None or in_a7 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
         return in_a1, in_a2, in_a6, in_a7
+
+    def st_test_bp(self) -> bool:
+        if self.st_test_10_bp():
+            if self.st_test_20_bp():
+                if self.st_test_30_bp():
+                    if self.st_test_40_bp():
+                        return True
+        return False
 
 
 if __name__ == '__main__':
+    test_bp = TestBP()
+    reset_test_bp = ResetRelay()
+    mysql_conn_bp = MySQLConnect()
+    fault = Bug(True)
     try:
-        test_bp = TestBP()
         if test_bp.st_test_bp():
-            mysql_conn.mysql_block_good()
+            mysql_conn_bp.mysql_block_good()
             my_msg('Блок исправен')
         else:
-            mysql_conn.mysql_block_bad()
-            my_msg('Блок неисправен')
+            mysql_conn_bp.mysql_block_bad()
+            my_msg('Блок неисправен', '#A61E1E')
     except OSError:
         my_msg("ошибка системы")
     except SystemError:
         my_msg("внутренняя ошибка")
+    except ModbusConnectException as mce:
+        fault.debug_msg(mce, 1)
+        my_msg(str(mce), '#A61E1E')
     finally:
-        reset.reset_all()
+        reset_test_bp.reset_all()
         exit()
