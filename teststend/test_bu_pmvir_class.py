06ab0af6704e64b1fcc04bf6ad66046318e34052
irik_safin <osiris365@gmail.com>
2022-03-01 22:27:37 +0500

    функции разбиты на более мелкие функции, глобальные переменные переведены в локальные, добавленны аннотации типов, == переделан в is, изменен цвет всплывающего окна при неисправности, добавлено исключение при отсутствии связи с контроллером

--- teststend/test_bu_pmvir_class.py
+++ teststend/test_bu_pmvir_class.py
@@ -7,9 +7,10 @@
 БУ ПМВИР (пускатель)	Без Производителя
 
 """
+import sys
 
-from sys import exit
 from time import sleep
+
 from gen_func_utils import *
 from my_msgbox import *
 from gen_mb_client import *
@@ -17,179 +18,229 @@ from gen_mysql_connect import *
 
 __all__ = ["TestBUPMVIR"]
 
-reset = ResetRelay()
-resist = Resistor()
-read_mb = ReadMB()
-ctrl_kl = CtrlKL()
-mysql_conn = MySQLConnect()
-fault = Bug(None)
-
 
 class TestBUPMVIR(object):
+
+    __resist = Resistor()
+    __read_mb = ReadMB()
+    __ctrl_kl = CtrlKL()
+    __mysql_conn = MySQLConnect()
+    __fault = Bug(None)
+
     def __init__(self):
         pass
     
-    def st_test_bu_pmvir(self):
-        # reset.reset_all()
-        # Тест 1. Проверка исходного состояния блока:
+    def st_test_10_bu_pmvir(self) -> bool:
+        """
+        Тест 1. Проверка исходного состояния блока:
+        """
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(89)
-            mysql_conn.mysql_ins_result("неисправен", '1')
+            self.__mysql_conn.mysql_error(89)
+            self.__mysql_conn.mysql_ins_result("неисправен", '1')
         # 1.1. Проверка состояния контактов блока при подаче напряжения питания
-        # opc['Устройство.tg.in_cont_ser_KT_KL27'] = True
-        ctrl_kl.ctrl_relay('KL21', True)
+        self.__ctrl_kl.ctrl_relay('KL21', True)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(90)
-            mysql_conn.mysql_ins_result("неисправен", '1')
+            self.__mysql_conn.mysql_error(90)
+            self.__mysql_conn.mysql_ins_result("неисправен", '1')
             return False
-        mysql_conn.mysql_ins_result("исправен", '1')
-        # 2. Проверка включения/выключения блока от кнопки «Пуск/Стоп».
-        # 2.1. Проверка исходного состояния блока
+        self.__mysql_conn.mysql_ins_result("исправен", '1')
+        return True
+
+    def st_test_20_bu_pmvir(self) -> bool:
+        """
+        2. Проверка включения/выключения блока от кнопки «Пуск/Стоп».
+        2.1. Проверка исходного состояния блока
+        """
         if self.__subtest_21():
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", '2')
+            self.__mysql_conn.mysql_ins_result("неисправен", '2')
             return False
         # 2.2. Выключение блока от кнопки «Стоп» при сопротивлении 10 Ом
-        ctrl_kl.ctrl_relay('KL12', False)
+        self.__ctrl_kl.ctrl_relay('KL12', False)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(92)
-            mysql_conn.mysql_ins_result("неисправен", '2')
+            self.__mysql_conn.mysql_error(92)
+            self.__mysql_conn.mysql_ins_result("неисправен", '2')
             return False
-        mysql_conn.mysql_ins_result("исправен", '2')
-        # 3. Проверка блокировки включения блока при снижении сопротивления изоляции контролируемого присоединения:
-        ctrl_kl.ctrl_relay('KL22', True)
-        resist.resist_ohm(10)
-        ctrl_kl.ctrl_relay('KL12', True)
+        self.__mysql_conn.mysql_ins_result("исправен", '2')
+        return True
+
+    def st_test_30_bu_pmvir(self) -> bool:
+        """
+        3. Проверка блокировки включения блока при снижении сопротивления изоляции контролируемого присоединения:
+        """
+        self.__ctrl_kl.ctrl_relay('KL22', True)
+        self.__resist.resist_ohm(10)
+        self.__ctrl_kl.ctrl_relay('KL12', True)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(93)
-            mysql_conn.mysql_ins_result("неисправен", '3')
+            self.__mysql_conn.mysql_error(93)
+            self.__mysql_conn.mysql_ins_result("неисправен", '3')
             return False
-        ctrl_kl.ctrl_relay('KL22', False)
-        ctrl_kl.ctrl_relay('KL12', False)
-        mysql_conn.mysql_ins_result("исправен", '3')
-        # 4.  Отключение блока при увеличении сопротивления цепи заземления на величину более 100 Ом
+        self.__ctrl_kl.ctrl_relay('KL22', False)
+        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.__mysql_conn.mysql_ins_result("исправен", '3')
+        return True
+
+    def st_test_40_bu_pmvir(self) -> bool:
+        """
+        4.  Отключение блока при увеличении сопротивления цепи заземления на величину более 100 Ом
+        """
         if self.__subtest_21():
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", '4')
+            self.__mysql_conn.mysql_ins_result("неисправен", '4')
             return False
-        resist.resist_10_to_137_ohm()
+        self.__resist.resist_10_to_137_ohm()
         sleep(1)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(94)
-            mysql_conn.mysql_ins_result("неисправен", '4')
+            self.__mysql_conn.mysql_error(94)
+            self.__mysql_conn.mysql_ins_result("неисправен", '4')
             return False
-        ctrl_kl.ctrl_relay('KL12', False)
-        mysql_conn.mysql_ins_result("исправен", '4')
-        # Тест 5. Защита от потери управляемости блока при замыкании проводов ДУ
+        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.__mysql_conn.mysql_ins_result("исправен", '4')
+        return True
+
+    def st_test_50_bu_pmvir(self) -> bool:
+        """
+        Тест 5. Защита от потери управляемости блока при замыкании проводов ДУ
+        """
         if self.__subtest_21():
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", '5')
+            self.__mysql_conn.mysql_ins_result("неисправен", '5')
             return False
-        ctrl_kl.ctrl_relay('KL11', True)
+        self.__ctrl_kl.ctrl_relay('KL11', True)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(95)
-            mysql_conn.mysql_ins_result("неисправен", '5')
+            self.__mysql_conn.mysql_error(95)
+            self.__mysql_conn.mysql_ins_result("неисправен", '5')
             return False
-        ctrl_kl.ctrl_relay('KL12', False)
-        ctrl_kl.ctrl_relay('KL11', False)
-        mysql_conn.mysql_ins_result("исправен", '5')
-        # Тест 6. Защита от потери управляемости блока при обрыве проводов ДУ
+        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.__ctrl_kl.ctrl_relay('KL11', False)
+        self.__mysql_conn.mysql_ins_result("исправен", '5')
+        return True
+
+    def st_test_60_bu_pmvir(self) -> bool:
+        """
+        Тест 6. Защита от потери управляемости блока при обрыве проводов ДУ
+        """
         if self.__subtest_21():
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", '6')
+            self.__mysql_conn.mysql_ins_result("неисправен", '6')
             return False
-        ctrl_kl.ctrl_relay('KL12', False)
+        self.__ctrl_kl.ctrl_relay('KL12', False)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(96)
-            mysql_conn.mysql_ins_result("неисправен", '6')
+            self.__mysql_conn.mysql_error(96)
+            self.__mysql_conn.mysql_ins_result("неисправен", '6')
             return False
-        mysql_conn.mysql_ins_result("исправен", '6')
-        # 7. Проверка отключения блока от срабатывания защиты УМЗ.
+        self.__mysql_conn.mysql_ins_result("исправен", '6')
+        return True
+
+    def st_test_70_bu_pmvir(self) -> bool:
+        """
+        7. Проверка отключения блока от срабатывания защиты УМЗ.
+        """
         if self.__subtest_21():
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен", '7')
+            self.__mysql_conn.mysql_ins_result("неисправен", '7')
             return False
-        ctrl_kl.ctrl_relay('KL27', False)
-        ctrl_kl.ctrl_relay('KL30', True)
+        self.__ctrl_kl.ctrl_relay('KL27', False)
+        self.__ctrl_kl.ctrl_relay('KL30', True)
         sleep(2)
-        ctrl_kl.ctrl_relay('KL27', True)
+        self.__ctrl_kl.ctrl_relay('KL27', True)
         sleep(6)
         in_a1 = self.__inputs_a()
-        if in_a1 == False:
+        if in_a1 is False:
             pass
         else:
-            mysql_conn.mysql_error(97)
-            mysql_conn.mysql_ins_result("неисправен", '7')
+            self.__mysql_conn.mysql_error(97)
+            self.__mysql_conn.mysql_ins_result("неисправен", '7')
             return False
-        ctrl_kl.ctrl_relay('KL30', False)
+        self.__ctrl_kl.ctrl_relay('KL30', False)
         sleep(6)
         in_a1 = self.__inputs_a()
-        if in_a1 == True:
+        if in_a1 is True:
             pass
         else:
-            mysql_conn.mysql_error(98)
-            mysql_conn.mysql_ins_result("неисправен", '7')
+            self.__mysql_conn.mysql_error(98)
+            self.__mysql_conn.mysql_ins_result("неисправен", '7')
             return False
-        mysql_conn.mysql_ins_result("исправен", '7')
+        self.__mysql_conn.mysql_ins_result("исправен", '7')
         return True
     
-    def __subtest_21(self):
-        # 2.1. Включение блока от кнопки «Пуск» при сопротивлении 10 Ом
-        resist.resist_ohm(10)
-        ctrl_kl.ctrl_relay('KL12', True)
+    def __subtest_21(self) -> bool:
+        """
+        2.1. Включение блока от кнопки «Пуск» при сопротивлении 10 Ом
+        """
+        self.__resist.resist_ohm(10)
+        self.__ctrl_kl.ctrl_relay('KL12', True)
         sleep(1)
         in_a1 = self.__inputs_a()
-        if in_a1 == True:
+        if in_a1 is True:
             return True
         else:
-            mysql_conn.mysql_error(91)
+            self.__mysql_conn.mysql_error(91)
             return False
     
-    @staticmethod
-    def __inputs_a():
-        in_a1 = read_mb.read_discrete(1)
+    def __inputs_a(self):
+        in_a1 = self.__read_mb.read_discrete(1)
+        if in_a1 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
         return in_a1
+
+    def st_test_bu_pmvir(self) -> bool:
+        if self.st_test_10_bu_pmvir():
+            if self.st_test_20_bu_pmvir():
+                if self.st_test_30_bu_pmvir():
+                    if self.st_test_40_bu_pmvir():
+                        if self.st_test_50_bu_pmvir():
+                            if self.st_test_60_bu_pmvir():
+                                if self.st_test_70_bu_pmvir():
+                                    return True
+        return False
 
 
 if __name__ == '__main__':
+    test_bu_pmvir = TestBUPMVIR()
+    reset_test_bu_pmvir = ResetRelay()
+    mysql_conn_bu_pmvir = MySQLConnect()
+    fault = Bug(True)
     try:
-        test_bu_pmvir = TestBUPMVIR()
         if test_bu_pmvir.st_test_bu_pmvir():
-            mysql_conn.mysql_block_good()
-            my_msg('Блок исправен')
+            mysql_conn_bu_pmvir.mysql_block_good()
+            my_msg('Блок исправен', '#1E8C1E')
         else:
-            mysql_conn.mysql_block_bad()
-            my_msg('Блок неисправен')
+            mysql_conn_bu_pmvir.mysql_block_bad()
+            my_msg('Блок неисправен', '#A61E1E')
     except OSError:
-        my_msg("ошибка системы")
+        my_msg("ошибка системы", '#A61E1E')
     except SystemError:
-        my_msg("внутренняя ошибка")
+        my_msg("внутренняя ошибка", '#A61E1E')
+    except ModbusConnectException as mce:
+        fault.debug_msg(mce, 1)
+        my_msg(str(mce), '#A61E1E')
     finally:
-        reset.reset_all()
-        exit()
+        reset_test_bu_pmvir.reset_all()
+        sys.exit()
