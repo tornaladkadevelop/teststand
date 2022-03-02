029fed21cd0e9d018ba5509e49c6270ec5888d55
irik_safin <osiris365@gmail.com>
2022-03-03 01:58:47 +0500

    функции разбиты на более мелкие функции, глобальные переменные переведены в локальные, добавленны аннотации типов, == переделан в is, изменен цвет всплывающего окна при неисправности, добавлено исключение при отсутствии связи с контроллером

--- teststend/test_buz_2_class.py
+++ teststend/test_buz_2_class.py
@@ -13,276 +13,316 @@
 
 __all__ = ["TestBUZ2"]
 
-from sys import exit
+import sys
+
 from time import sleep, time
+
 from my_msgbox import *
 from gen_func_procedure import *
 from gen_func_utils import *
 from gen_mb_client import *
 from gen_mysql_connect import *
 
-proc = Procedure()
-reset = ResetRelay()
-resist = Resistor()
-read_mb = ReadMB()
-mb_ctrl = CtrlKL()
-mysql_conn = MySQLConnect()
-fault = Bug(True)
-
-ust_1 = 75.8
-ust_2 = 20.3
-
 
 class TestBUZ2(object):
+
+    __proc = Procedure()
+    __reset = ResetRelay()
+    __read_mb = ReadMB()
+    __mb_ctrl = CtrlKL()
+    __mysql_conn = MySQLConnect()
+    __fault = Bug(True)
+
+    ust_1 = 75.8
+    ust_2 = 20.3
+
+    coef_volt: float
+
     def __init__(self):
         pass
     
-    def st_test_buz_2(self):
-        # reset.reset_all()
-        # Тест 1. Включение/выключение блока в нормальном режиме:
-        # Сообщение	Убедитесь в отсутствии блоков в панелях разъемов.
-        # Вставьте испытуемый блок БУЗ-2 в разъем Х17 на панели B.
-        # Вставьте заведомо исправные блок БИ в разъем Х26  и блок БДЗ в разъем Х16, расположенные на панели B.
+    @staticmethod
+    def st_test_10_buz_2() -> bool:
+        """
+        Тест 1. Включение/выключение блока в нормальном режиме:
+        """
         msg_1 = "Убедитесь в отсутствии блоков в панелях разъемов. " \
                 "Вставьте испытуемый блок БУЗ-2 в разъем Х17 на панели B."
         msg_2 = "Вставьте заведомо исправные блок БИ в разъем Х26  и блок БДЗ в разъем Х16, " \
                 "расположенные на панели B."
         if my_msg(msg_1):
             if my_msg(msg_2):
-                pass
-            else:
-                return False
-        else:
-            return False
-        # 1.1.	Проверка вероятности наличия короткого замыкания на входе измерительной цепи блока
-        fault.debug_msg("тестим шкаф", 3)
-        meas_volt_ust = proc.procedure_1_21_31()
-        if meas_volt_ust != False:
+                return True
+        return False
+
+    def st_test_11_buz_2(self) -> bool:
+        """
+        1.1.	Проверка вероятности наличия короткого замыкания на входе измерительной цепи блока
+        """
+        self.__fault.debug_msg("тестим шкаф", 3)
+        meas_volt_ust = self.__proc.procedure_1_21_31()
+        if meas_volt_ust is not False:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен TV1", "1")
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
             return False
-        mb_ctrl.ctrl_relay('KL73', True)
+        self.__mb_ctrl.ctrl_relay('KL73', True)
         sleep(5)
-        mb_ctrl.ctrl_relay('KL90', True)
+        self.__mb_ctrl.ctrl_relay('KL90', True)
         sleep(5)
-        mb_ctrl.ctrl_relay('KL63', True)
+        self.__mb_ctrl.ctrl_relay('KL63', True)
         min_volt = 0.6 * meas_volt_ust
         max_volt = 1.0 * meas_volt_ust
-        meas_volt = read_mb.read_analog()
-        fault.debug_msg(f'напряжение после включения KL63\t{meas_volt}\tдолжно быть от\t{min_volt}\tдо\t{max_volt}', 3)
+        meas_volt = self.__read_mb.read_analog()
+        self.__fault.debug_msg(f'напряжение после включения KL63\t{meas_volt}\t'
+                               f'должно быть от\t{min_volt}\tдо\t{max_volt}', 3)
         if min_volt <= meas_volt <= max_volt:
             pass
         else:
-            mysql_conn.mysql_ins_result('неисправен', '1')
-            mysql_conn.mysql_error(455)
-            reset.sbros_kl63_proc_1_21_31()
+            self.__mysql_conn.mysql_ins_result('неисправен', '1')
+            self.__mysql_conn.mysql_error(455)
+            self.__reset.sbros_kl63_proc_1_21_31()
             return False
-        reset.sbros_kl63_proc_1_21_31()
-        # 1.2. Определение коэффициента Кс отклонения фактического напряжения от номинального
-        fault.debug_msg("вычисляем коэффициент сети", 3)
-        coef_volt = proc.procedure_1_22_32()
-        if coef_volt != False:
+        self.__reset.sbros_kl63_proc_1_21_31()
+        return True
+
+    def st_test_12_buz_2(self) -> bool:
+        """
+        1.2. Определение коэффициента Кс отклонения фактического напряжения от номинального
+        """
+        self.__fault.debug_msg("вычисляем коэффициент сети", 3)
+        self.coef_volt = self.__proc.procedure_1_22_32()
+        if self.coef_volt is not False:
             pass
         else:
-            mysql_conn.mysql_ins_result("неисправен TV1", "1")
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
             return False
-        reset.stop_procedure_32()
-        fault.debug_msg("включаем хуеву тучу релюшек", 3)
-        mb_ctrl.ctrl_relay('KL21', True)
-        mb_ctrl.ctrl_relay('KL2', True)
-        mb_ctrl.ctrl_relay('KL66', True)
+        self.__reset.stop_procedure_32()
+        self.__fault.debug_msg("включаем хуеву тучу релюшек", 3)
+        self.__mb_ctrl.ctrl_relay('KL21', True)
+        self.__mb_ctrl.ctrl_relay('KL2', True)
+        self.__mb_ctrl.ctrl_relay('KL66', True)
         sleep(6)
-        mb_ctrl.ctrl_relay('KL84', True)
+        self.__mb_ctrl.ctrl_relay('KL84', True)
         sleep(2)
-        mb_ctrl.ctrl_relay('KL84', False)
+        self.__mb_ctrl.ctrl_relay('KL84', False)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL80', True)
+        self.__mb_ctrl.ctrl_relay('KL80', True)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL24', True)
+        self.__mb_ctrl.ctrl_relay('KL24', True)
         sleep(5)
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == True and in_a2 == True:
+        if in_a1 is True and in_a2 is True:
             pass
         else:
-            fault.debug_msg("тест 1.3 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "1")
+            self.__fault.debug_msg("тест 1.3 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
             return False
-        fault.debug_msg("тест 1.3 положение выходов соответствует", 4)
-        # 1.4.	Выключение блока
+        self.__fault.debug_msg("тест 1.3 положение выходов соответствует", 4)
+        return True
+
+    def st_test_13_buz_2(self) -> bool:
+        """
+        1.4.	Выключение блока
+        """
         sleep(1)
-        mb_ctrl.ctrl_relay('KL80', False)
+        self.__mb_ctrl.ctrl_relay('KL80', False)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL24', False)
+        self.__mb_ctrl.ctrl_relay('KL24', False)
         sleep(5)
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == False and in_a2 == False:
+        if in_a1 is False and in_a2 is False:
             pass
         else:
-            fault.debug_msg("тест 1.4 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "1")
+            self.__fault.debug_msg("тест 1.4 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "1")
             return False
-        fault.debug_msg("тест 1.4 положение выходов соответствует", 4)
-        mysql_conn.mysql_ins_result("исправен", "1")
-        # Тест 2. Проверка работоспособности защиты МТЗ:
-        # 2.1. Пуск блока
-        fault.debug_msg("тест 2 начало", 3)
-        mb_ctrl.ctrl_relay('KL66', False)
+        self.__fault.debug_msg("тест 1.4 положение выходов соответствует", 4)
+        self.__mysql_conn.mysql_ins_result("исправен", "1")
+        return True
+
+    def st_test_20_buz_2(self) -> bool:
+        """
+        Тест 2. Проверка работоспособности защиты МТЗ:
+        2.1. Пуск блока
+        """
+        self.__fault.debug_msg("тест 2 начало", 3)
+        self.__mb_ctrl.ctrl_relay('KL66', False)
         sleep(0.3)
-        mb_ctrl.ctrl_relay('KL82', True)
+        self.__mb_ctrl.ctrl_relay('KL82', True)
         sleep(0.3)
-        mb_ctrl.ctrl_relay('KL66', True)
+        self.__mb_ctrl.ctrl_relay('KL66', True)
         msg_3 = "Установите с помощью кнопок SB1, SB2 следующие уровни уставок: ПМЗ – 2000 А; ТЗП – 400 А"
         if my_msg(msg_3):
             pass
         else:
             return False
-        fault.debug_msg("включаем хуеву тучу релюшек", 3)
-        mb_ctrl.ctrl_relay('KL66', False)
+        self.__fault.debug_msg("включаем хуеву тучу релюшек", 3)
+        self.__mb_ctrl.ctrl_relay('KL66', False)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL82', False)
+        self.__mb_ctrl.ctrl_relay('KL82', False)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL66', True)
+        self.__mb_ctrl.ctrl_relay('KL66', True)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL84', True)
+        self.__mb_ctrl.ctrl_relay('KL84', True)
         sleep(2)
-        mb_ctrl.ctrl_relay('KL84', False)
+        self.__mb_ctrl.ctrl_relay('KL84', False)
         sleep(1)
-        mb_ctrl.ctrl_relay('KL80', True)
+        self.__mb_ctrl.ctrl_relay('KL80', True)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL24', True)
+        self.__mb_ctrl.ctrl_relay('KL24', True)
         sleep(2)
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == True and in_a2 == True:
+        if in_a1 is True and in_a2 is True:
             pass
         else:
-            fault.debug_msg("тест 2.1 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__fault.debug_msg("тест 2.1 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
             return False
-        fault.debug_msg("тест 2.1 положение выходов соответствует", 4)
-        if proc.start_procedure_1():
-            calc_volt = proc.start_procedure_24(coef_volt=coef_volt, setpoint_volt=ust_1)
-            if calc_volt != False:
-                if proc.start_procedure_34(calc_volt=calc_volt, setpoint_volt=ust_1):
-                    pass
-                else:
-                    mysql_conn.mysql_ins_result("неисправен TV1", "2")
-                    return False
-            else:
-                mysql_conn.mysql_ins_result("неисправен TV1", "2")
-                return False
+        self.__fault.debug_msg("тест 2.1 положение выходов соответствует", 4)
+        return True
+
+    def st_test_21_buz_2(self) -> bool:
+        if self.__proc.procedure_1_24_34(coef_volt=self.coef_volt, setpoint_volt=self.ust_1):
+            pass
         else:
-            mysql_conn.mysql_ins_result("неисправен TV1", "2")
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
             return False
-        mb_ctrl.ctrl_relay('KL63', True)
+        self.__mb_ctrl.ctrl_relay('KL63', True)
         sleep(0.5)
-        mb_ctrl.ctrl_relay('KL63', False)
+        self.__mb_ctrl.ctrl_relay('KL63', False)
         sleep(2)
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == False and in_a2 == False:
+        if in_a1 is False and in_a2 is False:
             pass
         else:
-            fault.debug_msg("тест 2.1 положение выходов не соответствует", 1)
-            reset.stop_procedure_3()
-            mysql_conn.mysql_ins_result("неисправен", "2")
+            self.__fault.debug_msg("тест 2.1 положение выходов не соответствует", 1)
+            self.__reset.stop_procedure_3()
+            self.__mysql_conn.mysql_ins_result("неисправен", "2")
             return False
-        fault.debug_msg("тест 2.1 положение выходов соответствует", 4)
-        reset.stop_procedure_3()
-        # 2.3.  Финишные операции при положительном завершении теста:
-        fault.debug_msg("включаем хуеву тучу релюшек", 3)
-        mb_ctrl.ctrl_relay('KL80', False)
-        mb_ctrl.ctrl_relay('KL24', False)
+        self.__fault.debug_msg("тест 2.1 положение выходов соответствует", 4)
+        self.__reset.stop_procedure_3()
+        return True
+
+    def st_test_22_buz_2(self) -> bool:
+        """
+        2.3.  Финишные операции при положительном завершении теста:
+        """
+        self.__fault.debug_msg("включаем хуеву тучу релюшек", 3)
+        self.__mb_ctrl.ctrl_relay('KL80', False)
+        self.__mb_ctrl.ctrl_relay('KL24', False)
         sleep(6)
-        mb_ctrl.ctrl_relay('KL84', True)
+        self.__mb_ctrl.ctrl_relay('KL84', True)
         sleep(2)
-        mb_ctrl.ctrl_relay('KL84', False)
+        self.__mb_ctrl.ctrl_relay('KL84', False)
         sleep(1)
-        mysql_conn.mysql_ins_result("исправен", "2")
-        # Тест 3. Проверка работоспособности защиты ТЗП
-        # 3.1. Пуск блока
-        mb_ctrl.ctrl_relay('KL80', True)
+        self.__mysql_conn.mysql_ins_result("исправен", "2")
+        return True
+
+    def st_test_30_buz_2(self) -> bool:
+        """
+        Тест 3. Проверка работоспособности защиты ТЗП
+        3.1. Пуск блока
+        """
+        self.__mb_ctrl.ctrl_relay('KL80', True)
         sleep(0.1)
-        mb_ctrl.ctrl_relay('KL24', True)
+        self.__mb_ctrl.ctrl_relay('KL24', True)
         sleep(2)
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == True and in_a2 == True:
+        if in_a1 is True and in_a2 is True:
             pass
         else:
-            fault.debug_msg("тест 3.1 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "3")
+            self.__fault.debug_msg("тест 3.1 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "3")
             return False
-        fault.debug_msg("тест 3.1 положение выходов соответствует", 4)
-        if proc.start_procedure_1():
-            calc_volt = proc.start_procedure_24(coef_volt=coef_volt, setpoint_volt=ust_2)
-            if calc_volt != False:
-                if proc.start_procedure_34(calc_volt=calc_volt, setpoint_volt=ust_2):
-                    pass
-                else:
-                    mysql_conn.mysql_ins_result("неисправен TV1", "3")
-                    return False
-            else:
-                mysql_conn.mysql_ins_result("неисправен TV1", "3")
-                return False
+        self.__fault.debug_msg("тест 3.1 положение выходов соответствует", 4)
+        return True
+
+    def st_test_31_buz_2(self) -> bool:
+        if self.__proc.procedure_1_24_34(coef_volt=self.coef_volt, setpoint_volt=self.ust_2):
+            pass
         else:
-            mysql_conn.mysql_ins_result("неисправен TV1", "3")
+            self.__mysql_conn.mysql_ins_result("неисправен", "3")
             return False
-        mb_ctrl.ctrl_relay('KL63', True)
+        self.__mb_ctrl.ctrl_relay('KL63', True)
         in_b1 = self.__inputs_b()
         k = 0
-        while in_b1 == False and k <= 10:
+        while in_b1 is False and k <= 10:
             in_b1 = self.__inputs_b()
             k += 1
         start_timer = time()
         in_a1 = self.__inputs_a1()
         stop_timer = 0
-        while in_a1 == True and stop_timer <= 360:
+        while in_a1 is True and stop_timer <= 360:
             in_a1 = self.__inputs_a1()
             stop_timer = time() - start_timer
-            fault.debug_msg(f'таймер тест 3.2 {stop_timer}', 2)
+            self.__fault.debug_msg(f'таймер тест 3.2 {stop_timer}', 2)
         timer_test_3 = stop_timer
         in_a1, in_a2 = self.__inputs_a()
-        if in_a1 == False and in_a2 == False and timer_test_3 <= 360:
+        if in_a1 is False and in_a2 is False and timer_test_3 <= 360:
             pass
         else:
-            fault.debug_msg("тест 3.2 положение выходов не соответствует", 1)
-            mysql_conn.mysql_ins_result("неисправен", "3")
-            reset.sbros_kl63_proc_all()
+            self.__fault.debug_msg("тест 3.2 положение выходов не соответствует", 1)
+            self.__mysql_conn.mysql_ins_result("неисправен", "3")
+            self.__reset.sbros_kl63_proc_all()
             return False
-        fault.debug_msg("тест 3.2 положение выходов соответствует", 4)
-        reset.sbros_kl63_proc_all()
-        mysql_conn.mysql_ins_result(f'исправен, {timer_test_3:.1f} сек', "3")
+        self.__fault.debug_msg("тест 3.2 положение выходов соответствует", 4)
+        self.__reset.sbros_kl63_proc_all()
+        self.__mysql_conn.mysql_ins_result(f'исправен, {timer_test_3:.1f} сек', "3")
         return True
     
-    @staticmethod
-    def __inputs_a():
-        in_a1 = read_mb.read_discrete(1)
-        in_a2 = read_mb.read_discrete(2)
+    def __inputs_a(self):
+        in_a1 = self.__read_mb.read_discrete(1)
+        in_a2 = self.__read_mb.read_discrete(2)
+        if in_a1 is None or in_a2 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
         return in_a1, in_a2
     
-    @staticmethod
-    def __inputs_a1():
-        in_a1 = read_mb.read_discrete(1)
+    def __inputs_a1(self):
+        in_a1 = self.__read_mb.read_discrete(1)
+        if in_a1 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
         return in_a1
 
-    @staticmethod
-    def __inputs_b():
-        in_a9 = read_mb.read_discrete(9)
+    def __inputs_b(self):
+        in_a9 = self.__read_mb.read_discrete(9)
+        if in_a9 is None:
+            raise ModbusConnectException(f'нет связи с контроллером')
         return in_a9
+
+    def st_test_buz_2(self) -> bool:
+        if self.st_test_10_buz_2():
+            if self.st_test_11_buz_2():
+                if self.st_test_12_buz_2():
+                    if self.st_test_13_buz_2():
+                        if self.st_test_20_buz_2():
+                            if self.st_test_21_buz_2():
+                                if self.st_test_22_buz_2():
+                                    if self.st_test_30_buz_2():
+                                        if self.st_test_31_buz_2():
+                                            return True
+        return False
 
 
 if __name__ == '__main__':
+    test_buz_2 = TestBUZ2()
+    reset_test_buz_2 = ResetRelay()
+    mysql_conn_buz_2 = MySQLConnect()
+    fault = Bug(True)
     try:
-        test_buz_2 = TestBUZ2()
         if test_buz_2.st_test_buz_2():
-            mysql_conn.mysql_block_good()
-            my_msg('Блок исправен')
+            mysql_conn_buz_2.mysql_block_good()
+            my_msg('Блок исправен', '#1E8C1E')
         else:
-            mysql_conn.mysql_block_bad()
-            my_msg('Блок неисправен')
+            mysql_conn_buz_2.mysql_block_bad()
+            my_msg('Блок неисправен', '#A61E1E')
     except OSError:
-        my_msg("ошибка системы")
+        my_msg("ошибка системы", '#A61E1E')
     except SystemError:
-        my_msg("внутренняя ошибка")
+        my_msg("внутренняя ошибка", '#A61E1E')
+    except ModbusConnectException as mce:
+        fault.debug_msg(mce, 1)
+        my_msg(str(mce), '#A61E1E')
     finally:
-        reset.reset_all()
-        exit()
+        reset_test_buz_2.reset_all()
+        sys.exit()
