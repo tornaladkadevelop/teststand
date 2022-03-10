d733304c4b6e66ddfabac55b54b527b986aca966
IRSafin <irik.safin@tornaladka.ru>
2022-03-10 22:33:48 +0500

    проведен тест на стенде, рабочий скрипт

--- teststend/test_bdu_014tp_class.py
+++ teststend/test_bdu_014tp_class.py
@@ -29,7 +29,6 @@ __all__ = ["TestBDU014TP"]
 
 
 class TestBDU014TP(object):
-
     __resist = Resistor()
     __ctrl_kl = CtrlKL()
     __read_mb = ReadMB()
@@ -38,12 +37,13 @@ class TestBDU014TP(object):
 
     def __init__(self):
         pass
-    
+
     def st_test_1_bdu_014tp(self) -> bool:
         """
         Тест 1. Проверка исходного состояния блока:
         :return:
         """
+        self.__fault.debug_msg(f'тест 1', 4)
         self.__mysql_conn.mysql_ins_result("идет тест 1", "1")
         in_a0, in_a1 = self.__inputs_a()
         if in_a1 is False:
@@ -60,12 +60,13 @@ class TestBDU014TP(object):
         Тест 2. Проверка включения / выключения блока от кнопки «Пуск / Стоп».
         2.1. Проверка исходного состояния блока
         """
+        self.__fault.debug_msg(f'тест 2.0', 4)
         self.__mysql_conn.mysql_ins_result("идет тест 2.1", "2")
         self.__ctrl_kl.ctrl_relay('KL2', True)
         in_a0, in_a1 = self.__inputs_a()
         sleep(1)
         if in_a1 is False:
-            pass
+            return True
         else:
             self.__mysql_conn.mysql_ins_result("неисправен", "2")
             return False
@@ -74,7 +75,8 @@ class TestBDU014TP(object):
         """
         2.2. Включение канала блока от кнопки «Пуск» при сопротивлении 10 Ом
         """
-        if self.__subtest_22(2.2, 2):
+        self.__fault.debug_msg(f'тест 2.1', 4)
+        if self.subtest_22(2.2, 2):
             return True
         return False
 
@@ -82,6 +84,7 @@ class TestBDU014TP(object):
         """
         2.3. Выключение канала блока от кнопки «Пуск» при сопротивлении 10 Ом
         """
+        self.__fault.debug_msg(f'тест 2.2', 4)
         self.__mysql_conn.mysql_ins_result("идет тест 2.3", "2")
         self.__ctrl_kl.ctrl_relay('KL12', False)
         sleep(1)
@@ -99,7 +102,8 @@ class TestBDU014TP(object):
         """
         повтор теста 2.2
         """
-        if self.__subtest_22(3.1, 3):
+        self.__fault.debug_msg(f'тест 3.0', 4)
+        if self.subtest_22(3.1, 3):
             return True
         return False
 
@@ -107,6 +111,7 @@ class TestBDU014TP(object):
         """
         3. Удержание исполнительного элемента при сопротивлении цепи заземления до 35 Ом
         """
+        self.__fault.debug_msg(f'тест 3.1', 4)
         self.__resist.resist_10_to_35_ohm()
         sleep(1)
         in_a0, in_a1 = self.__inputs_a()
@@ -123,8 +128,10 @@ class TestBDU014TP(object):
         """
         4. Отключение исполнительного элемента при сопротивлении цепи заземления свыше 50 Ом
         """
+        self.__fault.debug_msg(f'тест 4.0', 4)
         self.__mysql_conn.mysql_ins_result("идет тест 4", "4")
         self.__resist.resist_35_to_110_ohm()
+        sleep(1)
         in_a0, in_a1 = self.__inputs_a()
         if in_a1 is False:
             pass
@@ -141,7 +148,8 @@ class TestBDU014TP(object):
         """
         повтор теста 2.2
         """
-        if self.__subtest_22(5.1, 5):
+        self.__fault.debug_msg(f'тест 5.0', 4)
+        if self.subtest_22(5.1, 5):
             return True
         return False
 
@@ -149,7 +157,9 @@ class TestBDU014TP(object):
         """
         5. Защита от потери управляемости при замыкании проводов ДУ
         """
+        self.__fault.debug_msg(f'тест 5.1', 4)
         self.__ctrl_kl.ctrl_relay('KL11', True)
+        sleep(2)
         in_a0, in_a1 = self.__inputs_a()
         if in_a1 is False:
             pass
@@ -167,7 +177,8 @@ class TestBDU014TP(object):
         """
         повтор теста 2.2
         """
-        if self.__subtest_22(6.1, 6):
+        self.__fault.debug_msg(f'тест 6.0', 4)
+        if self.subtest_22(6.1, 6):
             return True
         return False
 
@@ -175,7 +186,9 @@ class TestBDU014TP(object):
         """
         Тест 6. Защита от потери управляемости при обрыве проводов ДУ
         """
+        self.__fault.debug_msg(f'тест 6.1', 4)
         self.__ctrl_kl.ctrl_relay('KL12', False)
+        sleep(1)
         in_a0, in_a1 = self.__inputs_a()
         if in_a1 is False:
             pass
@@ -185,13 +198,16 @@ class TestBDU014TP(object):
             return False
         self.__mysql_conn.mysql_ins_result("исправен", "6")
         return True
-    
-    def __subtest_22(self, subtest_2_num: float, test_2_num: int) -> bool:
+
+    def subtest_22(self, subtest_2_num: float, test_2_num: int) -> bool:
         self.__mysql_conn.mysql_ins_result(f'идёт тест {subtest_2_num}', f'{test_2_num}')
         self.__resist.resist_ohm(255)
+        # sleep(1)
         self.__resist.resist_ohm(10)
-        self.__ctrl_kl.ctrl_relay('KL12', True)
+        # sleep(2)
         self.__ctrl_kl.ctrl_relay('KL1', True)
+        sleep(1)
+        self.__ctrl_kl.ctrl_relay('KL12', True)
         sleep(1)
         in_a0, in_a1 = self.__inputs_a()
         if in_a1 is True:
@@ -203,10 +219,11 @@ class TestBDU014TP(object):
             return False
         self.__fault.debug_msg(f'тест {subtest_2_num} положение выходов соответствует', 3)
         return True
-    
+
     def __inputs_a(self):
         in_a0 = self.__read_mb.read_discrete(0)
         in_a1 = self.__read_mb.read_discrete(1)
+        self.__fault.debug_msg(f'{in_a0 = }  {in_a1 = }', 4)
         if in_a1 is None or in_a0 is None:
             self.__fault.debug_msg(f'нет связи с контроллером', 1)
         return in_a0, in_a1
@@ -234,17 +251,21 @@ if __name__ == '__main__':
     test_bdu_014tp = TestBDU014TP()
     reset_test_bdu_014tp = ResetRelay()
     mysql_conn_test_bdu_014tp = MySQLConnect()
+    fault = Bug(True)
     try:
         if test_bdu_014tp.st_test_bdu_014tp():
             mysql_conn_test_bdu_014tp.mysql_block_good()
-            my_msg('Блок исправен')
+            my_msg('Блок исправен', '#1E8C1E')
         else:
             mysql_conn_test_bdu_014tp.mysql_block_bad()
-            my_msg('Блок неисправен')
+            my_msg('Блок неисправен', '#A61E1E')
     except OSError:
-        my_msg("ошибка системы")
+        my_msg("ошибка системы", '#A61E1E')
     except SystemError:
-        my_msg("внутренняя ошибка")
+        my_msg("внутренняя ошибка", '#A61E1E')
+    except ModbusConnectException as mce:
+        fault.debug_msg(mce, 1)
+        my_msg(str(mce), '#A61E1E')
     finally:
         reset_test_bdu_014tp.reset_all()
         exit()
