f9cba9b6dbcf35dd33e753eafc7057ce3fdb5755
IRSafin <irik.safin@tornaladka.ru>
2022-03-12 22:36:43 +0500

    изменено сравнение процедур

--- teststend/test_btz_3_class.py
+++ teststend/test_btz_3_class.py
@@ -87,7 +87,7 @@ class TestBTZ3(object):
         """
         self.__mysql_conn.mysql_ins_result('идёт тест 1.1', '1')
         meas_volt_ust = self.__proc.procedure_1_21_31()
-        if meas_volt_ust is not False:
+        if meas_volt_ust != 0.0:
             pass
         else:
             self.__mysql_conn.mysql_error(433)
@@ -116,7 +116,7 @@ class TestBTZ3(object):
         self.__mysql_conn.mysql_ins_result('идёт тест 1.2', '1')
         self.__fault.debug_msg("1.2. Определение коэффициента Кс отклонения фактического напряжения от номинального", 3)
         self.coef_volt = self.__proc.procedure_1_22_32()
-        if self.coef_volt is not False:
+        if self.coef_volt != 0.0:
             pass
         else:
             self.__mysql_conn.mysql_ins_result("неисправен TV1", "1")
