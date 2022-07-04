972bd9cd28a72eeb6ea90c26424d0bd66e9a9e75
IRSafin <irik.safin@tornaladka.ru>
2022-07-05 01:04:36 +0500

    исправлены косяки в синтаксисе

--- teststend/gen_mysql_connect.py
+++ teststend/gen_mysql_connect.py
@@ -28,14 +28,14 @@ class MySQLConnect(object):
         :param num_test: номер теста
         :return:
         """
-        self.my_result = my_result
-        self.num_test = num_test
+        # my_result = my_result
+        # num_test = num_test
         try:
             conn = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
                                            database=self.database, auth_plugin=self.auth_plugin)
             c = conn.cursor()
-            retrive = ('UPDATE simple_database.test_results SET result_text = "' + self.my_result +
-                       '" WHERE id_test_result = "' + self.num_test + '"')
+            retrive = ('UPDATE simple_database.test_results SET result_text = "' + my_result +
+                       '" WHERE id_test_result = "' + num_test + '"')
             c.execute(retrive)
             conn.commit()
             conn.close()
@@ -48,7 +48,7 @@ class MySQLConnect(object):
         :param my_result:
         :return:
         """
-        self.my_result = my_result
+        # my_result = my_result
         sql = 'insert into pmz_result(pmz_set, pmz_proc, pmz_time) values(%s, %s, %s)'
 
         try:
@@ -71,7 +71,7 @@ class MySQLConnect(object):
         :param my_result:
         :return:
         """
-        self.my_result = my_result
+        # self.my_result = my_result
         sql = 'insert into tzp_result(tzp_set, tzp_proc, tzp_time) values(%s, %s, %s)'
 
         try:
@@ -80,7 +80,7 @@ class MySQLConnect(object):
             c = conn.cursor()
             aquery = 'TRUNCATE simple_database.tzp_result;'
             c.execute(aquery)
-            c.executemany(sql, self.my_result)
+            c.executemany(sql, my_result)
             conn.commit()
             print(c.rowcount, "\trecords inserted!")
             conn.close()
@@ -93,7 +93,7 @@ class MySQLConnect(object):
         :param my_result:
         :return:
         """
-        self.my_result = my_result
+        # self.my_result = my_result
         sql = 'insert into umz_result(umz_set_ab, umz_proc_ab, umz_time_ab, umz_set_vg, umz_proc_vg, umz_time_vg) ' + \
               'values(%s, %s, %s, %s, %s, %s)'
         try:
@@ -102,7 +102,7 @@ class MySQLConnect(object):
             c = conn.cursor()
             aquery = 'TRUNCATE simple_database.umz_result;'
             c.execute(aquery)
-            c.executemany(sql, self.my_result)
+            c.executemany(sql, my_result)
             conn.commit()
             print(c.rowcount, "records inserted!")
             conn.close()
@@ -115,7 +115,7 @@ class MySQLConnect(object):
         :param my_result:
         :return:
         """
-        self.my_result = my_result
+        # self.my_result = my_result
         sql = 'insert into ubtz_btz_result(ubtz_btz_ust, ubtz_btz_time) ' + 'values(%s, %s)'
         try:
             conn = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
@@ -123,7 +123,7 @@ class MySQLConnect(object):
             c = conn.cursor()
             aquery = 'TRUNCATE simple_database.ubtz_btz_result;'
             c.execute(aquery)
-            c.executemany(sql, self.my_result)
+            c.executemany(sql, my_result)
             conn.commit()
             print(c.rowcount, "records inserted!")
             conn.close()
@@ -136,7 +136,7 @@ class MySQLConnect(object):
         :param my_result:
         :return:
         """
-        self.my_result = my_result
+        # self.my_result = my_result
         sql = 'insert into ubtz_tzp_result(ubtz_tzp_ust, ubtz_tzp_time) ' + 'values(%s, %s)'
         try:
             conn = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
@@ -144,7 +144,7 @@ class MySQLConnect(object):
             c = conn.cursor()
             aquery = 'TRUNCATE simple_database.ubtz_tzp_result;'
             c.execute(aquery)
-            c.executemany(sql, self.my_result)
+            c.executemany(sql, my_result)
             conn.commit()
             print(c.rowcount, "records inserted!")
             conn.close()
@@ -157,12 +157,12 @@ class MySQLConnect(object):
         :param request:
         :return:
         """
-        self.request = request
+        # self.request = request
         try:
             conn = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
                                            database=self.database, auth_plugin=self.auth_plugin)
             c = conn.cursor()
-            c.execute(self.request)
+            c.execute(request)
             conn.commit()
             conn.close()
         except self.mysql_err:
@@ -194,7 +194,7 @@ class MySQLConnect(object):
         :param n_err: номер неисправности
         :return:
         """
-        self.n_err = n_err
+        # self.n_err = n_err
         upd = ('UPDATE simple_database.python_message SET pyth_error = "' + str(n_err) + '" WHERE id_pyth_mes = 1')
         self.mysql_connect(upd)
 
@@ -204,11 +204,11 @@ class MySQLConnect(object):
         :param mess: текстовое сообщение
         :return:
         """
-        self.mess = mess
+        # self.mess = mess
         mytime = datetime.now()
-        self.mess = self.mess[:170]
+        mess = mess[:170]
         request = "INSERT INTO simple_database.messages_alg(mess_alg_time, mess_text) " \
-                  "VALUES('" + str(mytime) + "','" + self.mess + "'); "
+                  "VALUES('" + str(mytime) + "','" + mess + "'); "
         print(request)
         try:
             conn = mysql.connector.connect(host=self.host, user=self.user, password=self.password,
