759d1ca295a4b9d607982e4911778e15e522ef01
IRSafin <irik.safin@tornaladka.ru>
2022-07-25 13:05:30 +0500

    общие методы сведены в одном файле, классе

--- teststend/test_bdu_r_t_class.py
+++ teststend/test_bdu_r_t_class.py
@@ -23,14 +23,14 @@ from gen_subtest import SubtestA1A2
 __all__ = ["TestBDURT"]
 
 
-class TestBDURT(object):
+class TestBDURT:
 
     def __init__(self):
-        self.__resist = Resistor()
-        self.__ctrl_kl = CtrlKL()
-        self.__read_mb = ReadMB()
-        self.__mysql_conn = MySQLConnect()
-        # self.__fault = Bug(None)
+        self.resist = Resistor()
+        self.ctrl_kl = CtrlKL()
+        self.read_mb = ReadMB()
+        self.mysql_conn = MySQLConnect()
+        # self.fault = Bug(None)
         self.subtest = SubtestA1A2()
 
         logging.basicConfig(filename="C:\Stend\project_class\log\TestBDURT.log",
@@ -42,8 +42,8 @@ class TestBDURT(object):
         self.logger = logging.getLogger(__name__)
 
     def st_test_1(self) -> bool:
-        if self.subtest.subtest_inp_a1a2(test_num=1, subtest_num=1.0, err_code_a1=288, err_code_a2=288,
-                                         position_a1=False, position_a2=False):
+        if self.subtest.subtest_2di(test_num=1, subtest_num=1.0, err_code_a1=288, err_code_a2=288, position_a1=False,
+                                    position_a2=False):
             return True
         return False
 
@@ -52,11 +52,11 @@ class TestBDURT(object):
         Тест 2. Проверка включения/выключения блока от кнопки «Пуск/Стоп» в режиме «Вперед».
         """
         self.logger.debug("старт теста 2.0")
-        self.__ctrl_kl.ctrl_relay('KL2', True)
+        self.ctrl_kl.ctrl_relay('KL2', True)
         self.logger.debug("включен KL2")
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=2, subtest_num=2.0, err_code_a1=290, err_code_a2=291,
-                                         position_a1=False, position_a2=False):
+        if self.subtest.subtest_2di(test_num=2, subtest_num=2.0, err_code_a1=290, err_code_a2=291, position_a1=False,
+                                    position_a2=False):
             return True
         return False
 
@@ -77,13 +77,13 @@ class TestBDURT(object):
         2.4. Выключение блока в режиме «Вперед» от кнопки «Стоп»
         """
         self.logger.debug("старт теста 2.3")
-        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.ctrl_kl.ctrl_relay('KL12', False)
         self.logger.debug(' включен KL12')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=2, subtest_num=2.3, err_code_a1=296, err_code_a2=297,
-                                         position_a1=False, position_a2=False):
-            self.__ctrl_kl.ctrl_relay('KL25', False)
-            self.__ctrl_kl.ctrl_relay('KL1', False)
+        if self.subtest.subtest_2di(test_num=2, subtest_num=2.3, err_code_a1=296, err_code_a2=297, position_a1=False,
+                                    position_a2=False):
+            self.ctrl_kl.ctrl_relay('KL25', False)
+            self.ctrl_kl.ctrl_relay('KL1', False)
             self.logger.debug(' отключены KL25, KL1')
             return True
         return False
@@ -94,14 +94,14 @@ class TestBDURT(object):
         3.1. Включение блока от кнопки «Пуск» в режиме «Назад»
         """
         self.logger.debug("старт теста 3.0")
-        self.__ctrl_kl.ctrl_relay('KL26', True)
+        self.ctrl_kl.ctrl_relay('KL26', True)
         self.logger.debug(' включен KL26')
-        self.__resist.resist_ohm(10)
-        self.__ctrl_kl.ctrl_relay('KL12', True)
+        self.resist.resist_ohm(10)
+        self.ctrl_kl.ctrl_relay('KL12', True)
         self.logger.debug(' включен KL12')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=3, subtest_num=3.0, err_code_a1=298, err_code_a2=299,
-                                         position_a1=False, position_a2=True):
+        if self.subtest.subtest_2di(test_num=3, subtest_num=3.0, err_code_a1=298, err_code_a2=299, position_a1=False,
+                                    position_a2=True):
             return True
         return False
 
@@ -111,13 +111,13 @@ class TestBDURT(object):
         при подключении Rш пульта управления:
         """
         self.logger.debug("старт теста 3.1")
-        self.__ctrl_kl.ctrl_relay('KL27', True)
-        self.__ctrl_kl.ctrl_relay('KL1', True)
-        self.__ctrl_kl.ctrl_relay('KL25', True)
+        self.ctrl_kl.ctrl_relay('KL27', True)
+        self.ctrl_kl.ctrl_relay('KL1', True)
+        self.ctrl_kl.ctrl_relay('KL25', True)
         self.logger.debug(' включены KL27, KL25, KL1')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=3, subtest_num=3.1, err_code_a1=300, err_code_a2=301,
-                                         position_a1=True, position_a2=False):
+        if self.subtest.subtest_2di(test_num=3, subtest_num=3.1, err_code_a1=300, err_code_a2=301, position_a1=True,
+                                    position_a2=False):
             return True
         return False
 
@@ -126,15 +126,15 @@ class TestBDURT(object):
         3.3. Выключение блока в режиме «Вперед» от кнопки «Стоп»
         """
         self.logger.debug("старт теста 3.2")
-        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.ctrl_kl.ctrl_relay('KL12', False)
         self.logger.debug(' отключен KL12')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=3, subtest_num=3.2, err_code_a1=302, err_code_a2=303,
-                                         position_a1=False, position_a2=False):
-            self.__ctrl_kl.ctrl_relay('KL26', False)
-            self.__ctrl_kl.ctrl_relay('KL27', False)
-            self.__ctrl_kl.ctrl_relay('KL1', False)
-            self.__ctrl_kl.ctrl_relay('KL25', False)
+        if self.subtest.subtest_2di(test_num=3, subtest_num=3.2, err_code_a1=302, err_code_a2=303, position_a1=False,
+                                    position_a2=False):
+            self.ctrl_kl.ctrl_relay('KL26', False)
+            self.ctrl_kl.ctrl_relay('KL27', False)
+            self.ctrl_kl.ctrl_relay('KL1', False)
+            self.ctrl_kl.ctrl_relay('KL25', False)
             self.logger.debug(' отключены KL26, KL27, KL1, KL25')
             return True
         return False
@@ -156,13 +156,13 @@ class TestBDURT(object):
         # 4. Отключение исполнительного элемента при увеличении сопротивления цепи заземления на величину свыше 50 Ом
         """
         self.logger.debug("старт теста 4.2")
-        self.__resist.resist_10_to_50_ohm()
+        self.resist.resist_10_to_50_ohm()
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=4, subtest_num=4.2, err_code_a1=304, err_code_a2=305,
-                                         position_a1=False, position_a2=False):
-            self.__ctrl_kl.ctrl_relay('KL12', False)
-            self.__ctrl_kl.ctrl_relay('KL25', False)
-            self.__ctrl_kl.ctrl_relay('KL1', False)
+        if self.subtest.subtest_2di(test_num=4, subtest_num=4.2, err_code_a1=304, err_code_a2=305, position_a1=False,
+                                    position_a2=False):
+            self.ctrl_kl.ctrl_relay('KL12', False)
+            self.ctrl_kl.ctrl_relay('KL25', False)
+            self.ctrl_kl.ctrl_relay('KL1', False)
             self.logger.debug(' отключены KL12, KL25, KL1')
             return True
         return False
@@ -184,15 +184,15 @@ class TestBDURT(object):
         # 5. Защита от потери управляемости при замыкании проводов ДУ
         """
         self.logger.debug("старт теста 5.2")
-        self.__ctrl_kl.ctrl_relay('KL11', True)
+        self.ctrl_kl.ctrl_relay('KL11', True)
         self.logger.debug(' включен KL11')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=5, subtest_num=5.2, err_code_a1=306, err_code_a2=307,
-                                         position_a1=False, position_a2=False):
-            self.__ctrl_kl.ctrl_relay('KL12', False)
-            self.__ctrl_kl.ctrl_relay('KL1', False)
-            self.__ctrl_kl.ctrl_relay('KL25', False)
-            self.__ctrl_kl.ctrl_relay('KL11', False)
+        if self.subtest.subtest_2di(test_num=5, subtest_num=5.2, err_code_a1=306, err_code_a2=307, position_a1=False,
+                                    position_a2=False):
+            self.ctrl_kl.ctrl_relay('KL12', False)
+            self.ctrl_kl.ctrl_relay('KL1', False)
+            self.ctrl_kl.ctrl_relay('KL25', False)
+            self.ctrl_kl.ctrl_relay('KL11', False)
             self.logger.debug(' отключены KL12, KL1, KL25, KL11')
             return True
         return False
@@ -214,11 +214,11 @@ class TestBDURT(object):
         # Тест 6. Защита от потери управляемости при обрыве проводов ДУ
         """
         self.logger.debug("старт теста 6.2")
-        self.__ctrl_kl.ctrl_relay('KL12', False)
+        self.ctrl_kl.ctrl_relay('KL12', False)
         self.logger.debug(' отключен KL12')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=6, subtest_num=6.2, err_code_a1=308, err_code_a2=309,
-                                         position_a1=False, position_a2=False):
+        if self.subtest.subtest_2di(test_num=6, subtest_num=6.2, err_code_a1=308, err_code_a2=309, position_a1=False,
+                                    position_a2=False):
             return True
         return False
 
@@ -227,11 +227,11 @@ class TestBDURT(object):
         # Тест 7. Проверка работоспособности функции "Проверка" блока
         """
         self.logger.debug("старт теста 7.0")
-        self.__ctrl_kl.ctrl_relay('KL24', True)
+        self.ctrl_kl.ctrl_relay('KL24', True)
         self.logger.debug(' включен KL24')
         sleep(1)
-        if self.subtest.subtest_inp_a1a2(test_num=7, subtest_num=7.0, err_code_a1=310, err_code_a2=311,
-                                         position_a1=False, position_a2=True):
+        if self.subtest.subtest_2di(test_num=7, subtest_num=7.0, err_code_a1=310, err_code_a2=311, position_a1=False,
+                                    position_a2=True):
             return True
         return False
 
@@ -261,7 +261,7 @@ if __name__ == '__main__':
     test_bdu_r_t = TestBDURT()
     reset_test_bdu_r_t = ResetRelay()
     mysql_conn_bdu_r_t = MySQLConnect()
-    fault = Bug(True)
+    fault = Bug(None)
     try:
         if test_bdu_r_t.st_test_bdu_r_t():
             mysql_conn_bdu_r_t.mysql_block_good()
